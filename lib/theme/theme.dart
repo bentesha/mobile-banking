1e5b0ca2960221244393956b98df7dd2c82970a6
= <benedict.tesha@jamaatech.com>
2021-03-12 15:43:14 +0300

    adapt app theme to MCB branding

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
 
-  static final primaryColor = Color(0xff097ec2);
-  static final secondaryColor = Color(0xffee3235);
-  static final accentColor = Color(0xfff5ec3c);
+  static final primaryColor = Color(0xff0f4f9a);
+  static final secondaryColor = Color(0xffe97926);
+  static final accentColor = Color(0xffe97926);
 
   static ThemeData create() => ThemeData(
     primaryColor: primaryColor,
